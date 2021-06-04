8a9b466721c7cf8c0e3226be4a755fa87d5e2fc3
Adityask49 <85333215+Adityask49@users.noreply.github.com>
2021-06-04 10:59:16 +0400

    Update wallis.c

--- wallis.c
+++ wallis.c
@@ -23,4 +23,24 @@ int main(void) {
     }
   }
 }
+float mc_pi(int n)
+{ float t,x,y;
+ int within_circle=0;
+ int outside_circle=0;
+ int total_point=0;
+ for(int i=0;i<=n;i++)
+ { x=frrandom();
+  y=frandom();
+  t=(x*x)+(y*y);
+  if(t<=1)
+  { within_circle++;
+  }
+  else
+  { outside_circle++;
+  }
+ }
+ total_points= within_circle+ outside_circle;
+ float pi= 4*(float)within_circle/total_points;
+ return pi;
+}
 
